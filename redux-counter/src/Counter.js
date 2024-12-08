e12098f3551eb2b863b7ea37361a88516e01963d
Yedan Li <63386101+liyedanpdx@users.noreply.github.com>
2024-12-07 20:49:21 -0600

    redux

--- redux-counter/src/Counter.js
+++ redux-counter/src/Counter.js
@@ -1,30 +1,37 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class Counter extends React.Component{
     state={count:0}
     increment=()=>{
-        this.setState({
-            count: this.state.count+1
-        });
+        this.props.dispatch({type:'INCREMENT'});
     }
     decrement=()=>{
-        this.setState({
-            count: this.state.count-1
-        });
+        this.props.dispatch({type:'DECREMENT'});
+    }
+    reset=()=>{
+        this.props.dispatch({type:'RESET'})
     }
     render(){
         return(
             <div>
-                <h2>Counter</h2>
-                <div>
-                    <span>{this.state.count}</span>
-                    <button onClick={this.decrement}>-</button>
-                    <button onClick={this.increment}>+</button>
+                <h2 class="fw-bold">Counter</h2>
+                <div style={{backgroundColor: "#00CCFF"}} class="p-3">
+                    <span className="border border-white text-white p-1">{this.props.count}</span>
+                    <button class="mx-2" onClick={this.decrement}>-</button>
+                    <button class="mx-2" onClick={this.increment}>+</button>
+                    <button class="mx-2" onClick={this.reset}>reset</button>
                 </div>
             </div>
         )
     }
 }
 
+function mapStateToProps(state){
+    return {
+        count: state.count
+    }
+}
 
-export default Counter;
+export default connect(mapStateToProps)(Counter);
